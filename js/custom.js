d46178e37f7c0c764f6f6dd7282691cb709f542f
Florian ALONSO <alonso.floo@gmail.com>
2015-03-27 11:29:45 +0100

    Adding analytics event tracker

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,18 @@
  * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
  * File Description: Place here your custom scripts
  */
+
+
+$(document).ready(function() {
+    $(".overlay-container").click(function() {
+        var title = $(this).text().trim();
+        alert(title);
+        ga('send', 'event', 'project', 'seen', title);
+    });
+});
+    
+function resumeButton(url) {
+    ga('send', 'event', 'resume', 'download');
+    var win = window.open(url, '_blank');
+    win.focus();
+}
